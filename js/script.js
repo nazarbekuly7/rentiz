9a70c2affcf0f2264abfb0bf59761fb8f22ee63b
nazarbekuly7 <tifixgame@gmail.com>
2023-11-05 17:21:36 +0600

    section hero

--- js/script.js
+++ js/script.js
@@ -9,3 +9,64 @@ if (burger && menu) {
         body.classList.toggle('_lock');
     })
 }
+
+const filter = document.querySelector('.filter');
+
+if (filter) {
+    const items = filter.querySelectorAll('.block-filter')
+
+    items.forEach(item => {
+        item.addEventListener('click', event => {
+            item.querySelector('.block-filter__dropdown').classList.toggle('_active');
+            item.querySelector('.block-filter__icon').classList.toggle('_active');
+
+            if (event.target.classList.contains('block-filter__item')) {
+                item.querySelector('.block-filter__value').textContent = event.target.textContent;
+            }
+        })
+    })
+}
+
+
+const popularSlider = new Swiper('.popular-slider', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.popular-slider-next',
+        prevEl: '.popular-slider-prev',
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+        },
+        660: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+const reviewsSlider = new Swiper('.slider-reviews', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    autoHeight: true,
+    navigation: {
+        nextEl: '.slider-reviews-next',
+        prevEl: '.slider-reviews-prev',
+    },
+});
+
+
+const galleryItems = document.querySelectorAll(".gallery__item");
+
+if (galleryItems.length > 0) {
+    galleryItems.forEach(item => {
+        new Swiper(item, {
+            slidesPerView: 1,
+            autoplay: {
+                delay: 5000,
+            },
+            effect: 'fade',
+        })
+    })
+}
